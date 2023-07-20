2d43496187aa8f5ec4ea492f9666bdaefd34736c
Iraklu <chaxva32@gmail.com>
2023-07-20 13:04:57 -0700

    just testing out

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,16 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'StalkCord',
-  description: 'Stalk Anyone you Want',
+  openGraph: {
+    type: "website",
+    url: "https://stalk-cord.vercel.app/",
+    title: "StalkCord",
+    description: "Stalk Anyone From Discord",
+    siteName: "StalkCord",
+    images: [{
+      url: "https://imgtr.ee/images/2023/07/16/853d6b58c99809d3bb41fe08ef5da8bd.png",
+    }],
+  }
 }
 
 export default function RootLayout({
@@ -16,6 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
       <body className={inter.className}>{children}</body>
     </html>
   )
